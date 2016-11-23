216ea1d7b2a33c84db8eddcdced47a756a90192b
leiyong0326 <282211932@qq.com>
2016-11-23 23:21:29 +0800

    first commit

--- phone-admin/target/m2e-wtp/web-resources/src/js/vip/vehicle.js
+++
@@ -1,9 +0,0 @@
-define([ '../require.config' ], function(r) {
-
-	require([ 'jquery', 'utils', 'template', 'pagination', 'datetimepicker', 'navbar', 'amazeui' ], function($, utils, Temp, Page, datepicker) {
-
-		
-
-	});
-
-});
--- phone-admin/target/m2e-wtp/web-resources/unauthorized.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h2>Hello World!</h2>
-</body>
-/unauthorized.jsp = anon
-</html>
